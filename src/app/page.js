67f12894aa8ce51f76b9093c16e7e557281b5549
Michael Claybaugh <mclaybaugh2@liberty.edu>
2024-01-10 22:16:30 -0500

    the basis with text

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,26 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { data } from './data';
 
 export default function Home() {
+  const lines = data.inthebeginning.map((words) => {
+    const wordEls = words.map(word => {
+      return (
+        <div>
+          {word}
+        </div>
+      )
+    })
+    return (
+      <div className={styles.verse}>
+        {wordEls}
+      </div>
+    );
+  })
   return (
-    <div>Hi There</div>
+    <div className={styles.text}>
+      {lines}
+    </div>
   )
 }
+
